a838a34d637ed73fd71909087418344fe29a9d85
Quirin <quirin.ecker@bajupa.com>
2019-10-01 09:25:08 +0200

    update

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>3ahitm-medt-u3-comments_and_variables</title>
+</head>
 
-?>
+<body>
+    <?php
+        $liter1 = 40.5;
+        $liter2 = 35.7;
+        $price = 1.499;
+        $cost = 0;
+
+        // Mit der Addition und der Multiplikation wird der insgesammte preis ausgerechnet
+        $cost = $liter1 * $price + $liter2 * $price;
+
+        $output = "Die Benzinkosten betragen für " . ($liter1 + $liter2) . "Liter $cost €";
+
+        echo $output;
+    ?>
+</body>
+</html>
+
+
